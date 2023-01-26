0e1d810e670245aea30901425cd2787819f1bdd7
uch0731 <uch0731@naver.com>
2023-01-26 15:24:52 +0900

    2

---
+++ src/main/java/com/shop/controller/ThymeLeafExController.java
@@ -0,0 +1,17 @@
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/thymeleaf")
+public class ThymeLeafExController {
+
+    @GetMapping(value = "/ex01")
+    public String thymeleafExample01(Model model){
+        model.addAttribute("data", "타임리프 예제 ㅋ");
+        return "thymeleafEx/thymeleafEx01";
+    }
+}
